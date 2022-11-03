233e7723ae2b848a69bd434a2cb749f62692234b
Hugo Latreille <hugo.latreille@gmail.com>
2022-11-03 09:01:32 +0100

    :wrench:config: setup webpack/bootstrap/sass

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,7 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
